0af314bc50d7937b461c16869ef8543040d027e9
DucPhan <skynext00@gmail.com>
2023-10-10 14:47:54 +0700

    Text1

--- src/App.js
+++ src/App.js
@@ -1,20 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [step, setStep] = useState(0);
+  useEffect(() => {
+    setTimeout(() => {
+      setStep(1);
+    }, 2000);
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Duc gl
-        </a>
-      </header>
+      {step == 0 ? (
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Duc Pro
+          </a>
+        </header>
+      ) : null}
+      {step == 1 ? (
+        <div className="Container">
+          <a className="Text">Hòa ghẻ, Hưng gà vào đây bố cân 2</a>
+        </div>
+      ) : null}
     </div>
   );
 }
