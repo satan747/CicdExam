4947c638bbbee4e5a30d2ccfcc9172d04e46addf
DucPhan <skynext00@gmail.com>
2023-10-10 14:54:34 +0700

    text1 update3

--- src/App.js
+++ src/App.js
@@ -3,32 +3,9 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [step, setStep] = useState(0);
-  useEffect(() => {
-    setTimeout(() => {
-      setStep(1);
-    }, 2000);
-  }, []);
-
   return (
     <div className="App">
-      {step == 0 ? (
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Duc Pro
-          </a>
-        </header>
-      ) : (
-        <div className="Container">
-          <a className="Text">Hoa ghe, Hung ga`</a>
-        </div>
-      )}
+      <a className="Text">Hoa ghe, Hung ga`</a>
     </div>
   );
 }
